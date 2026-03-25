0939916651dc5b2c389674f2a49519e52c62efd9
KhanhDuy <nguyenkhanhduy.29042004tv@gmail.com>
2026-03-25 15:59:56 +0700

    add item_layout for each product in recycle view

--- app/src/main/java/com/example/restaurantapp2/ListFragment.kt
+++ app/src/main/java/com/example/restaurantapp2/ListFragment.kt
@@ -3,4 +3,5 @@ package com.example.restaurantapp2
 import androidx.fragment.app.Fragment
 
 class ListFragment: Fragment(R.layout.fragment_list){
+
 }
---
+++ app/src/main/java/com/example/restaurantapp2/models/Product.kt
@@ -0,0 +1,4 @@
+package com.example.restaurantapp2.models
+
+class Product {
+}
