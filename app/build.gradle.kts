c5bf5895c59d2974d5136e4062a0e2fe0f301265
KhanhDuy <nguyenkhanhduy.29042004tv@gmail.com>
2026-04-08 02:22:44 +0700

    set up some google login feature

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,6 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
+
 }
--- app/src/main/java/com/example/restaurantapp2/LoginActivity.kt
+++ app/src/main/java/com/example/restaurantapp2/LoginActivity.kt
@@ -2,13 +2,19 @@ package com.example.restaurantapp2
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.restaurantapp2.Role.Admin.AdminActivity
 import com.example.restaurantapp2.Role.Customer.CustomerActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +27,30 @@ class LoginActivity : AppCompatActivity() {
             insets
         }
 
+
+
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+//            .requestIdToken("1025657299481-sd6ei3fvd1bn68ncnklb161agkh2eic0.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+
+        val googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        val btnLoginAsGoogle = findViewById<View>(R.id.btnLoginAsGoogle)
+        btnLoginAsGoogle.setOnClickListener {
+            val signInIntent = googleSignInClient.signInIntent
+            startActivityForResult(signInIntent, 100) // RC_SIGN_IN = 100
+
+        }
+
+
+
+
+
+
+        //un-comment this code and change the layout to activity_login to use the quick login buttons for admin and customer
+
 //        val btnAdmin = findViewById<Button>(R.id.admin_button)
 //        val btnCustomer = findViewById<Button>(R.id.customer_button)
 //
@@ -32,6 +62,28 @@ class LoginActivity : AppCompatActivity() {
 //            val intent = Intent(this, CustomerActivity::class.java)
 //            startActivity(intent)
 //        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 100) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                val name = account.displayName
+                val email = account.email
+                val idToken = account.idToken
+
+                Toast.makeText(this, "Logged in as $name ($email)", Toast.LENGTH_LONG).show()
+                Log.d("GoogleLogin", "ID Token: $idToken")
+
+            } catch (e: ApiException) {
+                Toast.makeText(this, "Sign in failed: ${e.statusCode}", Toast.LENGTH_LONG).show()
+                Log.d("GoogleLogin", "Sign in failed: ${e.statusCode}")
+            }
+        }
     }
 
 
